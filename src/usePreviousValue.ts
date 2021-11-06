6b0bf296d04c5dc2e8f2f9e8db4b2bb6513fb24b
ashalfarhan <ashalfarhan8@gmail.com>
2021-11-06 13:21:25 +0700

    prettier

--- src/usePreviousValue.ts
+++ src/usePreviousValue.ts
@@ -2,13 +2,13 @@ import * as React from 'react';
 
 /**
  * Get previous value of state change
- * 
+ *
  * @param value any value
  * @returns previous value of `value` and possibly `undefined` at fist render
  */
 export function usePreviousValue<T>(value: T) {
   const ref = React.useRef<T>();
-  
+
   React.useEffect(() => {
     ref.current = value;
   }, [value]);
